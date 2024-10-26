0b3cfafe3d4f2c5717817af585915f0953ab412e
pablosegundo31 <pablosegundolobos18@gmail.com>
2024-10-26 00:10:03 -0300

    carga de ejemplos

---
+++ index.mjs
@@ -0,0 +1,8 @@
+import {leerSuperheroes} from "./utils.mjs";
+
+//leer y mostrar la lista de superheroes ordenada
+const superheroes = leerSuperheroes ('./superheroes.txt');
+console.log('Superheroes ordenados:');
+console.log(superheroes);
+
+
