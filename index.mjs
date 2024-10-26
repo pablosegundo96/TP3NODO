79b84f6b642c3606837f64255899f5b80d3584f9
pablosegundo31 <pablosegundolobos18@gmail.com>
2024-10-26 13:24:39 -0300

    carga

--- index.mjs
+++ index.mjs
@@ -1,8 +1,15 @@
-import {leerSuperheroes} from "./utils.mjs";
+import {agregarSuperheroes, leerSuperheroes} from "./utils.mjs";
 
-//leer y mostrar la lista de superheroes ordenada
-const superheroes = leerSuperheroes ('./superheroes.txt');
+const archivoOriginal = './superherores.txt';
+const archivoNuevos = './agregarSuperherores.txt';
+
+//agregar neuvos superherores
+agregarSuperheroes(archivoOriginal, archivoNuevos);
+
+//leer y mostrar la lista actualizada de superheroes ordenada
+const superheroes = leerSuperheroes(archivoOriginal);
 console.log('Superheroes ordenados:');
 console.log(superheroes);
+
 
 
